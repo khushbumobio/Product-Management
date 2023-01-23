d04a93ed845a428383f5ba14e13f94d9640af467
khushbu <khushbu.waghela@mobiosolutions.com>
2023-01-23 11:55:49 +0530

    categhory management running

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -1,7 +1,9 @@
 const userRouter = require('./userRoutes')
 const authRouter = require('./authRoutes')
+const categoryRouter = require('./categoryRoutes')
 
 module.exports = (app) => {
     app.use('/auth', authRouter);
-    app.use('/user',userRouter)
+    app.use('/user',userRouter);
+    app.use('/category',categoryRouter);
 }
