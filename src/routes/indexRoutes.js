80644507af84a388812091cc00d04caf55396dcc
khushbu <khushbu.waghela@mobiosolutions.com>
2023-02-01 14:01:01 +0530

    product management completed

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -1,11 +1,13 @@
 const authRouter = require('./authRoutes')
 const userRouter =require('./userRoutes')
 const categoryRouter = require('./categoryRoutes')
+const productRouter = require('./productRoutes')
 
 
 module.exports = (app) => {
     app.use('/auth', authRouter);
     app.use('/user',userRouter);
     app.use('/category',categoryRouter);
+    app.use('/product',productRouter);
 
 }
